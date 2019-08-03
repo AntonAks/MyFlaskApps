0540ccde970a99eb23a3a4578041b76039b539dc
AntonAks <antonaks84@gmail.com>
2019-08-03 15:51:49 +0300

    add new application (Simple Quiz)

---
+++ SimpleQuiz/start_quiz.py
@@ -0,0 +1,31 @@
+"""
+Simple Quiz test 
+This App start Flask application with simple quiz.
+Questions and answers stored in JSON file
+"""
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    """
+    Index function for render main start page
+    """
+    return render_template('index.html')
+
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz_page():
+    """
+    Function for render quiz test page 
+    """
+    return render_template('quiz_page.html')
+
+
+if __name__ == '__main__':
+
+    app.debug = False
+    HOST = os.environ.get('IP', '127.0.0.1')
+    PORT = int(os.environ.get('PORT', 8080))
+    app.run(host=HOST, port=PORT)
