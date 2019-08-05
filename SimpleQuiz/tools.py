73a176166ccdc66f874cec0d97005d7dc480d5f8
AntonAks <antonaks84@gmail.com>
2019-08-05 14:41:10 +0300

    add corrections by pylint module (PEP8)

--- SimpleQuiz/tools.py
+++ SimpleQuiz/tools.py
@@ -1,3 +1,9 @@
+"""
+Tools module used for establish backend engine for quiz functionality
+1. class for handling questions
+2. load data from json storage
+3. store results history
+"""
 import json
 import os
 import random
@@ -6,7 +12,9 @@ BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__
 QUESTIONS_FILE = os.path.join(BASE_DIR, 'questions.json')
 
 class Question:
-
+    """
+    Class Question used for handle a question instance and for load questions from json storage
+    """
     def __init__(self):
         self.text_summary = ''
         self.answers = {}
@@ -16,6 +24,10 @@ class Question:
 
     @staticmethod
     def get_questions():
+        """
+        loads questions data from json storage
+        return: shuffled list of question objects
+        """
         with open(QUESTIONS_FILE, 'r') as json_file:
             questions_json_data = json.load(json_file)
 
